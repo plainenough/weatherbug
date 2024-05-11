ad854bafd7e7752db8ed2553b0133e722b552af0
Derrick <derrick.walton@ymail.com>
2024-05-11 20:29:19 +0000

    fix: remove redundant calls

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -2,29 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-
-module "ecr" {
-  source           = "../../modules/ecr"
-  environment_name = var.environment_name
-}
-
-
-module "iam" {
-  source = "../../modules/iam"
-}
-
-
-module "vpc" {
-  source           = "../../modules/vpc"
-  environment_name = var.environment_name
-  region           = var.region
-
-}
-
-
 module "eks" {
   source           = "../../modules/eks"
-  environment_name = var.environmnent_name
+  environment_name = var.environment_name
   region           = var.region
 }
 
