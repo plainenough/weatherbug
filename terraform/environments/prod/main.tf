3ddd2b12adabd931298d1449cd48185be3de71b2
Derrick <derrick.walton@ymail.com>
2024-05-11 19:14:35 +0000

    fix: missing region

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -17,10 +17,14 @@ module "iam" {
 module "vpc" {
   source    = "../../modules/vpc"
   environment_name = "prod"
+  region = "us-east-2"
+
 }
 
 
 module "eks" {
   source           = "../../modules/eks"
+  environment_name = "prod"
+  region = "us-east-2"
 }
 
