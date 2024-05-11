6aab3a64a3ef43740e2201b3a6833b76acbd0354
Derrick <derrick.walton@ymail.com>
2024-05-11 19:15:17 +0000

    fix: add kubectl and helm to setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,10 +4,20 @@ set -eo pipefail
 echo "Installing Utilities"
 
 echo "Setup hachicorp repository...."
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+#wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-echo "Update and install terraform...."
-sudo apt update && sudo apt install -y terraform
+echo "Update and install terraform awscli...."
+sudo apt update && sudo apt install -y terraform awscli
+
+echo "Setting up kubectl...."
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Setting up helm...."
+wget -O helm.tar.gz https://get.helm.sh/helm-v3.14.4-linux-amd64.tar.gz
+tar -zxvf helm.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+rm -rf linux-amd64 kubectl helm.tar.gz
 
 echo "Done installing utilities...."
