082d2bb5582c5f43f9e48152d7c05d730acbe3f7
Derrick <derrick.walton@ymail.com>
2024-05-11 19:18:45 +0000

    chore: update format

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -3,35 +3,35 @@ resource "aws_vpc" "public_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "VPC-${var.region}-${var.environment_name}"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "VPC-${var.region}-${var.environment_name}"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
 
 resource "aws_subnet" "public_subnets" {
-  count      = length(var.public_subnet_cidrs)
-  vpc_id     = aws_vpc.public_vpc.id
-  cidr_block = var.public_subnet_cidrs[count.index]
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.public_vpc.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = "${var.region}${var.azs[count.index]}"
   tags = {
-    Name = "Public-${var.environment_name}-${var.region}-${var.azs[count.index]}"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "Public-${var.environment_name}-${var.region}-${var.azs[count.index]}"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
 
 resource "aws_subnet" "private_subnets" {
-  count      = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.public_vpc.id
-  cidr_block = var.private_subnet_cidrs[count.index]
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.public_vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = "${var.region}${var.azs[count.index]}"
   tags = {
-    Name = "Private-${var.environment_name}-${var.region}-${var.azs[count.index]}"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"    
+    Name      = "Private-${var.environment_name}-${var.region}-${var.azs[count.index]}"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
@@ -40,9 +40,9 @@ resource "aws_subnet" "private_subnets" {
 resource "aws_eip" "nat_a" {
   domain = "vpc"
   tags = {
-    Name = "EIP for NAT Gateway AZ-a"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "EIP for NAT Gateway AZ-a"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
@@ -50,9 +50,9 @@ resource "aws_eip" "nat_a" {
 resource "aws_eip" "nat_b" {
   domain = "vpc"
   tags = {
-    Name = "EIP for NAT Gateway AZ-b"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "EIP for NAT Gateway AZ-b"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
@@ -60,9 +60,9 @@ resource "aws_eip" "nat_b" {
 resource "aws_eip" "nat_c" {
   domain = "vpc"
   tags = {
-    Name = "EIP for NAT Gateway AZ-c"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "EIP for NAT Gateway AZ-c"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
 }
 
@@ -70,21 +70,21 @@ resource "aws_eip" "nat_c" {
 resource "aws_internet_gateway" "public_gateway" {
   vpc_id = aws_vpc.public_vpc.id
   tags = {
-    Name = "Project VPC IG"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
- }
+    Name      = "Project VPC IG"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
+  }
 }
 
 
 # This assumes that subnets order is maintained throughout creation.
 resource "aws_nat_gateway" "nat_1" {
   allocation_id = aws_eip.nat_a.id
-  subnet_id     = aws_subnet.private_subnets[0].id  
+  subnet_id     = aws_subnet.private_subnets[0].id
   tags = {
-    Name = "NAT Gateway 1"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "NAT Gateway 1"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
   depends_on = [aws_internet_gateway.public_gateway, aws_eip.nat_a]
 }
@@ -92,11 +92,11 @@ resource "aws_nat_gateway" "nat_1" {
 
 resource "aws_nat_gateway" "nat_2" {
   allocation_id = aws_eip.nat_b.id
-  subnet_id     = aws_subnet.private_subnets[1].id  
+  subnet_id     = aws_subnet.private_subnets[1].id
   tags = {
-    Name = "NAT Gateway 2"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "NAT Gateway 2"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
   depends_on = [aws_internet_gateway.public_gateway, aws_eip.nat_b]
 }
@@ -104,28 +104,28 @@ resource "aws_nat_gateway" "nat_2" {
 
 resource "aws_nat_gateway" "nat_3" {
   allocation_id = aws_eip.nat_c.id
-  subnet_id     = aws_subnet.private_subnets[2].id  
+  subnet_id     = aws_subnet.private_subnets[2].id
   tags = {
-    Name = "NAT Gateway 3"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "NAT Gateway 3"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
   depends_on = [aws_internet_gateway.public_gateway, aws_eip.nat_c]
 }
 
 
 resource "aws_route_table" "public_route_table" {
- vpc_id = aws_vpc.public_vpc.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.public_gateway.id
- }
+  vpc_id = aws_vpc.public_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_gateway.id
+  }
   tags = {
-    Name = "Public Route Table"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
- } 
- depends_on = [aws_internet_gateway.public_gateway]
+    Name      = "Public Route Table"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
+  }
+  depends_on = [aws_internet_gateway.public_gateway]
 }
 
 
@@ -133,7 +133,7 @@ resource "aws_route_table" "private" {
   count  = 3
   vpc_id = aws_vpc.public_vpc.id
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = element([
       aws_nat_gateway.nat_1.id,
       aws_nat_gateway.nat_2.id,
@@ -141,13 +141,13 @@ resource "aws_route_table" "private" {
     ], count.index)
   }
   tags = {
-    Name = "Private-Subnet-RouteTable-${count.index}"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "Private-Subnet-RouteTable-${count.index}"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
-  depends_on = [      aws_nat_gateway.nat_1,
-      aws_nat_gateway.nat_2,
-      aws_nat_gateway.nat_3]
+  depends_on = [aws_nat_gateway.nat_1,
+    aws_nat_gateway.nat_2,
+  aws_nat_gateway.nat_3]
 }
 
 
@@ -164,11 +164,11 @@ resource "aws_security_group" "external_web_traffic_sg" {
   vpc_id      = aws_vpc.public_vpc.id
 
   tags = {
-    Name = "ExternalWebTrafficSecurityGroup"
-    Project     = "weatherbug"
-    ManagedBy   = "terraform"
+    Name      = "ExternalWebTrafficSecurityGroup"
+    Project   = "weatherbug"
+    ManagedBy = "terraform"
   }
-  depends_on = [ aws_vpc.public_vpc ]
+  depends_on = [aws_vpc.public_vpc]
 }
 
 
@@ -188,7 +188,7 @@ resource "aws_security_group_rule" "allow_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  # Allows traffic from any IP address
+  cidr_blocks       = ["0.0.0.0/0"] # Allows traffic from any IP address
   security_group_id = aws_security_group.external_web_traffic_sg.id
   description       = "Allow inbound HTTPS traffic"
 }
@@ -213,7 +213,7 @@ resource "aws_security_group" "eks_node_sg" {
   tags = {
     Name = "eks-worker-node-security-group"
   }
-  depends_on = [ aws_vpc.public_vpc ]
+  depends_on = [aws_vpc.public_vpc]
 }
 
 
@@ -245,5 +245,5 @@ resource "aws_security_group_rule" "efs_access" {
   to_port           = 2049
   protocol          = "tcp"
   security_group_id = aws_security_group.eks_node_sg.id
-  cidr_blocks       = var.private_subnet_cidrs 
+  cidr_blocks       = var.private_subnet_cidrs
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,21 +1,21 @@
 variable "public_subnet_cidrs" {
- type        = list(string)
- description = "Public Subnet CIDR values"
- default     = ["10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28"]
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28"]
 }
 
- 
+
 variable "private_subnet_cidrs" {
- type        = list(string)
- description = "Private Subnet CIDR values"
- default     = ["10.0.0.48/28", "10.0.0.64/28", "10.0.0.80/28"]
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.0.48/28", "10.0.0.64/28", "10.0.0.80/28"]
 }
 
 
 variable "azs" {
- type        = list(string)
- description = "Availability Zones"
- default     = ["a", "b", "c"]
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["a", "b", "c"]
 }
 
 
