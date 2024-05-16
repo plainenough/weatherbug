f7e97742f2dfce692fb9ef4f411318bf06678821
Derrick <derrick.walton@ymail.com>
2024-05-16 02:52:59 +0000

    fix: add another rule to security group

--- terraform/modules/vpc/security-groups/main.tf
+++ terraform/modules/vpc/security-groups/main.tf
@@ -124,6 +124,16 @@ resource "aws_security_group_rule" "eks_control_plane_ingress" {
   description       = "Allow traffic from the worker nodes"
 }
 
+resource "aws_security_group_rule" "worker_control_plane_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_node_sg.id
+  source_security_group_id = aws_security_group.eks_node_sg.id
+  description       = "Allow traffic from the worker nodes"
+}
+
 
 resource "aws_security_group" "public_sg" {
   name        = "${var.environment_name}-public-sg"
