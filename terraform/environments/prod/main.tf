16a312f0b7e2e8864fd8c44da339900a22231cb4
Derrick <derrick.walton@ymail.com>
2024-05-30 15:04:47 +0000

    fix: wire everything in the environment file

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -2,9 +2,28 @@ provider "aws" {
   region = var.region
 }
 
+
+module "vpc" {
+  source = "../../modules/vpc"
+  region = var.region
+  environment_name = var.environment_name
+}
+
+
+module "ecr" {
+  source = "../../modules/ecr"
+  environment_name = var.environment_name
+}
+
+
 module "eks" {
   source           = "../../modules/eks"
   environment_name = var.environment_name
   region           = var.region
+  ecr_repository_arn = module.ecr.ecr_repository_arn
+  depends_on = [
+    module.ecr,
+    module.vpc
+  ]
 }
 
