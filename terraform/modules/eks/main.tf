5618a466e435368e3a5646faa3b86d7480c48a9f
Derrick <derrick.walton@ymail.com>
2024-05-30 20:15:52 +0000

    fix: remove uneeded policy

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -123,34 +123,9 @@ resource "aws_eks_addon" "cni_addon" {
 }
 
 
-resource "aws_iam_role" "eks_kube_proxy_role" {
-  name = "eks-kube-proxy-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_role_policy_attachment" "eks_kube_proxy_AmazonEKS_KubeProxyPolicy" {
-  role       = aws_iam_role.eks_kube_proxy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_KubeProxyPolicy"
-}
-
-
 resource "aws_eks_addon" "kubeproxy_addon" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "kube-proxy"
-  service_account_role_arn = aws_iam_role.eks_kube_proxy_role.arn
 }
 
 
